df316ccf29d7bbf05d9933b74a4967fa3ebab9eb
Nicolas MARCEL <nicolas2.marcel2004@gmail.com>
2026-03-03 12:31:48 +0100

    updated plot_error.py to take in account multiple measurements files

--- sim/LELO_GR01/plot_error.py
+++ sim/LELO_GR01/plot_error.py
@@ -1,11 +1,10 @@
-#Read the frequency measurements of the oscillator at different frequencies from the files oscillator.csv and plot the linearity errors
+#Read the frequency measurements of the oscillator at different frequencies from different csv files and plot the errors of temperature measurements
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-filename = "data.csv"  #data file
 measurement_time = 1/32768             #Slice time in which we count the oscillations to measure frequency
 
 
@@ -14,39 +13,44 @@ def parse_data(filename):
     with open(filename, 'r') as f:
         lines = f.read().split("\n")
 
-
     measurements = []
-    for i in range(1, len(lines)):
+    for i in range(len(lines)):
         line_split = lines[i].split(";")
         if len(line_split) == 2:
-            measurements.append([float(line_split[0]), float(line_split[1])])
+            measurements.append([float(line_split[0]), float(line_split[1])]) 
 
-    measurements  = np.array(measurements)
-
-    return measurements
+    return np.array(measurements)
 
 
 
+data_files = ["tran_SchGtKssTtVl", "tran_SchGtKttTlVt", "tran_SchGtKffTlVt"]
+#data_files = ["../data"]
 
+measurements_list = []
+for file in data_files:
+    measurements_list.append(parse_data("output_tran/"+file+".csv"))
 
-
-
-measurements = parse_data(filename)
-x = measurements[:, 0]
-y = measurements[:, 1]
 
 
 #---- Compute the linear regression of the mesurements ----#
-a, b = np.polyfit(x, y, 1)
+a_list = []
+b_list = []
+for i in range(len(measurements_list)):
+    a, b = np.polyfit(measurements_list[i][:, 0], measurements_list[i][:, 1], 1)
+    a_list.append(a)
+    b_list.append(b)
+
 
 
 
 #---- Plotting the linear regression and the measurements ----#
-x_line = np.linspace(x.min(), x.max(), 10)
-y_line = a * x_line + b
 plt.figure()
-plt.scatter(x, y, color="blue", label="Measured frequency")
-plt.plot(x_line, y_line, color="red", label=f"y = {a:.2f}x + {b:.2f}")
+for i in range(len(measurements_list)):
+    x_line = np.linspace(measurements_list[i][:, 0].min(), measurements_list[i][:, 0].max(), 10)
+    y_line = a_list[i] * x_line + b_list[i]
+    plt.scatter(measurements_list[i][:, 0], measurements_list[i][:, 1], label=data_files[i])
+    plt.plot(x_line, y_line)
+
 plt.xlabel("Temperature (°C)")
 plt.ylabel("Frequency (Hz)")
 plt.title("Frequency measurements")
@@ -55,11 +59,46 @@ plt.grid(True)
 
 
 
+
+
+
+
+#---- Computing measurements errors ----#
+measured_temperature_error_list = []
+for i in range(len(measurements_list)):
+    measured_temperature = (measurements_list[i][:, 1]-b_list[i])/a_list[i]
+    measured_temperature_error_list.append(measured_temperature - measurements_list[i][:, 0])
+
+
+
+
+#---- Computing for each temperature the max and min error ----#
+y_max = []
+y_min = []
+for i in range(len(measurements_list[0][:, 0])):
+    y_max.append(measured_temperature_error_list[0][i])
+    y_min.append(measured_temperature_error_list[0][i])
+    for j in range(len(measured_temperature_error_list)):
+        if y_max[-1] < measured_temperature_error_list[j][i]:
+            y_max[-1] = measured_temperature_error_list[j][i]
+        if y_min[-1] > measured_temperature_error_list[j][i]:
+            y_min[-1] = measured_temperature_error_list[j][i]
+
+
+
+
 #---- Plotting the measured temperature error ----#
-measured_temperature = (y-b)/a
-measured_temperature_error = measured_temperature - x
+PLOT_ONLY_TYPICAL=True #Assuming that typical is the the data_files list
 plt.figure()
-plt.plot(x, measured_temperature_error, color="blue", label="error", marker='o')
+plt.fill_between(measurements_list[0][:, 0], y_max, y_min, alpha=0.3, label="Area where all the measurements fit in")
+
+if PLOT_ONLY_TYPICAL:
+    plt.plot(measurements_list[0][:, 0], measured_temperature_error_list[i], label=data_files[i], marker='o')
+else:
+    for i in range(len(measurements_list)):
+        plt.plot(measurements_list[i][:, 0], measured_temperature_error_list[i], label=data_files[i], marker='o')
+
+
 plt.xlabel("Temperature (°C)")
 plt.ylabel("Temperature measurements error (°C)")
 plt.title("Temperature measurements error")
@@ -67,6 +106,9 @@ plt.legend()
 plt.grid(True)
 
 
+
+
+'''
 #---- Plotting the measured temperature error without and with the quantification noise ----#
 number_oscillations = (measurement_time * y).astype(int)    #Number of oscillations that occur during the measurement time
 a_q, b_q = np.polyfit(x, number_oscillations, 1)            #Compute a new linear regression with a quantified number of oscillations
@@ -81,11 +123,9 @@ plt.title("Temperature estimation error")
 plt.legend()
 plt.grid(True)
 
-
+'''
 
 
 
 
 plt.show()
-
-#print(measurements)
